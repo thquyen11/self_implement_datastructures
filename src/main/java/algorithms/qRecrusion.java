10f9b8e0f8fbfb3d09a15174b476e7ee8b41bb55
thquyen11 <thquyen11@hotmail.com>
2018-11-15 20:34:32 -0500

    add recursion implement

---
+++ src/main/java/algorithms/qRecrusion.java
@@ -0,0 +1,10 @@
+package algorithms;
+
+public class qRecrusion {
+	
+	public static Integer findFactorialR(Integer input) {
+		if(input==0) return 1;
+		
+		return input*findFactorialR(input-1);
+	}
+}
---
+++ src/test/java/algorithms/test_qRecursion.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+public class test_qRecursion {
+
+	public static void main(String[] args) {
+
+		System.out.println(qRecrusion.findFactorialR(0));
+		System.out.println(qRecrusion.findFactorialR(5));
+
+	}
+
+}
